3aad3b289559b08c2df9398c9dbcfc22b7574450
Nello Casolla <nellocasolla446@gmail.com>
2023-03-13 15:18:48 +0100

    second

--- index.js
+++ index.js
@@ -18,17 +18,47 @@ class User {
   }
   
   // Creating objects from the User class
-  const user1 = new User("John", "Doe", 30, "New York");
-  const user2 = new User("Jane", "Smith", 25, "Los Angeles");
+  const user1 = new User("Victor", "Osimhen", 30, "New York");
+  const user2 = new User("Kvicha", "Kvaraschtelia", 25, "Georgia");
   
   // Testing the compareAge method
-  console.log(user1.compareAge(user2)); // Output: "John is older than Jane"
-  
+  console.log(user1.compareAge(user2)); // Output: "1 is older than 2"
 
-  crea un form per la creazione di oggetti "Pet". 
-  la classe Pet dovrà essere dotata delle seguenti proprietà:
-  -petName
-  -ownerName
-  -species 
-  -breed 
-  Nella classe che utilizzerai per creare questi oggetti aggiungi anche un 
+  class Pet {
+    constructor(petName, ownerName, species, breed) {
+      this.petName = petName;
+      this.ownerName = ownerName;
+      this.species = species;
+      this.breed = breed;
+    }
+  
+    hasSameOwner(otherPet) {
+      return this.ownerName === otherPet.ownerName;
+    }
+  }
+  
+  const pets = [];
+  
+  function addPet() {
+    const petName = document.getElementById("pet-name").value;
+    const ownerName = document.getElementById("owner-name").value;
+    const species = document.getElementById("species").value;
+    const breed = document.getElementById("breed").value;
+  
+    const pet = new Pet(petName, ownerName, species, breed);
+    pets.push(pet);
+  
+    displayPets();
+  }
+  
+  function displayPets() {
+    const petList = document.getElementById("pet-list");
+    petList.innerHTML = "";
+  
+    pets.forEach(pet => {
+      const li = document.createElement("li");
+      li.textContent = `${pet.petName} - ${pet.ownerName} - ${pet.species} - ${pet.breed}`;
+      petList.appendChild(li);
+    });
+  }
+  
